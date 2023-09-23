563f32ef2a66a27e976d7c0b8ce5b1435578252d
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-09-23 13:45:21 +0700

    updating routes.js and README.md

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # hapi-web-server-dicoding
 Learning Course Dicoding In Class 'Belajar Membuat Aplikasi Back-End untuk Pemula dengan Google Cloud'
+
+## Run Server
+1. Open a terminal or command prompt on your computer.
+2. Run the following command to run it:
+   ```shell
+   npm run start
--- routes.js
+++ routes.js
@@ -21,10 +21,17 @@ const routes = [
         },
     },
     {
-        method: '*',
-        path: '/about',
+        method: 'GET',
+        path: '/hello/{name?}',
         handler: (request, h) => {
-            return 'Halaman tidak dapat diakses dengan method tersebut';
+            const { name = "stranger" } = request.params;
+            const { lang } = request.query;
+
+            if (lang === 'id') {
+                return `Hai, ${name}!`;
+            }
+
+            return `Hello, ${name}!`;
         },
     },
     {
